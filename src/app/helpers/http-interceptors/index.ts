3c54d3892eddc85b4bc3f98cfc4e8398845bd81a
miguelhenao <miguel.henao@utp.edu.co>
2022-03-11 23:03:40 -0500

    Delete interceptor

--- src/app/helpers/http-interceptors/index.ts
+++
@@ -1,5 +0,0 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { TenantInterceptor } from './tenant.interceptor';
-
-export const httpInterceptorProviders = [{ provide: HTTP_INTERCEPTORS, useClass: TenantInterceptor, multi: true }];
