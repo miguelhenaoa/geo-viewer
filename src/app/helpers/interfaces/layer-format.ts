2df037e50ad1b98e9c91beca0efef24a0face72f
miguelhenao <miguel.henao@utp.edu.co>
2022-03-15 00:25:58 -0500

    Define formats layer

---
+++ src/app/helpers/interfaces/layer-format.ts
@@ -0,0 +1,19 @@
+export enum TypeLayer {
+  ShapeFile = 'ShapeFile',
+  Csv = 'CSV',
+  Gpx = 'GPX',
+  GeoJSON = 'GeoJSON'
+}
+
+export interface FormatLayer {
+  name: string;
+  type: string;
+  extension: string;
+}
+
+export const FORMATS = {
+  [TypeLayer.ShapeFile]: { name: TypeLayer.ShapeFile, type: 'application/x-zip-compressed', extension: 'shapefile' },
+  [TypeLayer.Csv]: { name: TypeLayer.Csv, type: 'csv', extension: 'csv' },
+  [TypeLayer.Gpx]: { name: TypeLayer.Gpx, type: 'gpx', extension: 'gpx' },
+  [TypeLayer.GeoJSON]: { name: TypeLayer.GeoJSON, type: 'json', extension: 'json' }
+};
